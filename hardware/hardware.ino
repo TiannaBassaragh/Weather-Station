8b95062832429148781c99b42609fc7ab0bd8868
TiannaBassaragh <tianna.bassaragh@outlook.com>
2025-03-18 18:44:02 -0500

    added moisture sensor

--- hardware/hardware.ino
+++ hardware/hardware.ino
@@ -6,9 +6,12 @@
 
 // DEFINITIONS
 #define DHT_TYPE DHT22
+#define MOISTURE_MAX 3600 //I might have to lower it
+#define MOISTURE_MIN 1457
 
 // Pin Definitions
 #define DHT_PIN 14
+#define MOISTURE_PIN 34
 
 // Object Initialization
 DHT dht(DHT_PIN, DHT_TYPE);
@@ -17,18 +20,18 @@ Adafruit_BMP280 bmp;
 // Structure for sensor readings
 struct SensorData
 {
-    float cTemp;        //DHT
-    float humidity;     //DHT
-    float heatIndex;    //DHT
-    float pressure;     //BMP
-    float altitude;     //BMP
-    bool isValid;       //to tell if the data retrieved is valid
+    float cTemp;            //DHT
+    float humidity;         //DHT
+    float heatIndex;        //DHT
+    float pressure;         //BMP
+    float altitude;         //BMP
+    float moisturePercent;  //Capacitive Soil Moisture Sensor
+    bool  isValid;          //to tell if the data retrieved is valid
 };
 
 // Function Declarations
 SensorData readSensors();
 void displayData(SensorData data);
-
 
 
 void setup() {
@@ -47,13 +50,15 @@ void setup() {
         Adafruit_BMP280::SAMPLING_X16,      // Best pressure accuracy
         Adafruit_BMP280::FILTER_X16,        // Best smoothing to ignore small fluctuations in the air
         Adafruit_BMP280::STANDBY_MS_500);   // Able to update every second
+    
+    pinMode(MOISTURE_PIN, INPUT);
 }
 
 void loop() {
     SensorData info = readSensors();
     displayData (info);
 
-    delay(2000);
+    delay(1000);
 }
 
 SensorData readSensors() {
@@ -64,9 +69,20 @@ SensorData readSensors() {
     data.pressure = bmp.readPressure() / 100.0; // Convert from Pa to hPa
     data.altitude = bmp.readAltitude(1013.25);  // Standard pressure at sea level
 
+    // data.moisturePercent = analogRead(MOISTURE_PIN);
+    int moistureValue = analogRead(MOISTURE_PIN);
+    data.moisturePercent = map(moistureValue, 3600, 1457, 0, 100);
+
     //Ensures readings logged are valid
-    if (isnan(data.humidity)|| isnan(data.cTemp)){
+    if (isnan(data.humidity)||isnan(data.cTemp)){
+        Serial.println("Failed to read from DHT sensor.");
         data.isValid = false;
+    } else if (isnan(data.pressure)||isnan(data.altitude)) {
+        Serial.println("Failed to read from BMP sensor.");
+        data.isValid = false;
+    // } else if ((data.moisturePercent>MOISTURE_MAX)||(data.moisturePercent<MOISTURE_MIN)) {
+    //     Serial.println("Failed to read from Capacitive Soil Moisture sensor.");
+    //     data.isValid = false;
     } else {
         data.isValid = true;
     }
@@ -76,7 +92,6 @@ SensorData readSensors() {
 
 void displayData(SensorData data){
     if(!data.isValid){
-        Serial.println("Failed to read from DHT sensor.");
         return;
     }
     
@@ -90,5 +105,8 @@ void displayData(SensorData data){
     Serial.print(data.pressure);
     Serial.print(" hPa, Altitude: ");
     Serial.print(data.altitude);
-    Serial.println(" m");
+    Serial.print(" m, Soil Moisture: ");
+    Serial.print(data.moisturePercent);
+    Serial.println("%");
+    Serial.println("");
 }
