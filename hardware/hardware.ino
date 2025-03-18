a7deab0009f5057e66763551ddecdc57f387db69
TiannaBassaragh <tianna.bassaragh@outlook.com>
2025-03-18 16:46:22 -0500

    Included BMP sensor

--- hardware/hardware.ino
+++ hardware/hardware.ino
@@ -2,6 +2,7 @@
 #include <rom/rtc.h>    //for ESP32 dev board
 
 #include <DHT.h>
+#include <Adafruit_BMP280.h>
 
 // DEFINITIONS
 #define DHT_TYPE DHT22
@@ -9,27 +10,43 @@
 // Pin Definitions
 #define DHT_PIN 14
 
+// Object Initialization
+DHT dht(DHT_PIN, DHT_TYPE);
+Adafruit_BMP280 bmp;
+
 // Structure for sensor readings
 struct SensorData
 {
-    float cTemp;
-    float humidity;
-    float heatIndex;
-    bool isValid;
+    float cTemp;        //DHT
+    float humidity;     //DHT
+    float heatIndex;    //DHT
+    float pressure;     //BMP
+    float altitude;     //BMP
+    bool isValid;       //to tell if the data retrieved is valid
 };
 
 // Function Declarations
 SensorData readSensors();
-void displayData( SensorData data);
+void displayData(SensorData data);
 
-// Object Initialization
-DHT dht(DHT_PIN, DHT_TYPE);
 
 
 void setup() {
     Serial.begin(115200);
 
     dht.begin();
+
+    if (!bmp.begin(0x76)) {
+        Serial.println("Could not find valid BMP280 sensor.");
+        while(1);
+    }
+
+    bmp.setSampling(
+        Adafruit_BMP280::MODE_NORMAL, 
+        Adafruit_BMP280::SAMPLING_X16,      // Most accurate temperature reading
+        Adafruit_BMP280::SAMPLING_X16,      // Best pressure accuracy
+        Adafruit_BMP280::FILTER_X16,        // Best smoothing to ignore small fluctuations in the air
+        Adafruit_BMP280::STANDBY_MS_500);   // Able to update every second
 }
 
 void loop() {
@@ -44,6 +61,8 @@ SensorData readSensors() {
     data.humidity = dht.readHumidity();
     data.cTemp = dht.readTemperature(); //Temperature in Celsius
     data.heatIndex = dht.computeHeatIndex(data.cTemp, data.humidity, false);
+    data.pressure = bmp.readPressure() / 100.0; // Convert from Pa to hPa
+    data.altitude = bmp.readAltitude(1013.25);  // Standard pressure at sea level
 
     //Ensures readings logged are valid
     if (isnan(data.humidity)|| isnan(data.cTemp)){
@@ -67,5 +86,9 @@ void displayData(SensorData data){
     Serial.print(data.humidity);
     Serial.print(" %, Heat Index: ");
     Serial.print(data.heatIndex);
-    Serial.println(" °C");
+    Serial.print(" °C, Pressure: ");
+    Serial.print(data.pressure);
+    Serial.print(" hPa, Altitude: ");
+    Serial.print(data.altitude);
+    Serial.println(" m");
 }
