705e1e85bf3b68b433d4d9495b258192f1e9a1d6
TiannaBassaragh <tianna.bassaragh@outlook.com>
2025-03-21 23:01:59 -0500

    Updated backend (Still works)

--- hardware/hardware.ino
+++ hardware/hardware.ino
@@ -94,8 +94,8 @@ static const char* mqtt_server   = "www.yanacreations.com";         // Broker IP
 static uint16_t mqtt_port        = 1883;
 
 // WIFI CREDENTIALS
-const char* ssid       = "TA"; // Add your Wi-Fi ssid //MonaConnect
-const char* password   = "trollarmy"; // Add your Wi-Fi password 
+const char* ssid       = "MonaConnect"; // Add your Wi-Fi ssid //MonaConnect
+const char* password   = ""; // Add your Wi-Fi password 
 
 // // TASK HANDLES 
 TaskHandle_t xMQTT_Connect          = NULL; 
